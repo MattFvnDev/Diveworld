51b32711c0682f347301905b034ff18882d3b50c
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-17 22:23:27 +0200

    Defining navigationLinks

---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+export const navigationLinks = [
+    {
+      id: "0",
+      title: "Oferta",
+      url: "/oferta",
+    },
+    {
+      id: "1",
+      title: "Szkolenia",
+      url: "/szkolenia",
+    },
+    {
+      id: "2",
+      title: "Oferta Grupowa",
+      url: "/oferta-grupowa",
+    },
+    {
+      id: "3",
+      title: "O Nas",
+      url: "o-nas",
+    },
+    {
+      id: "4",
+      title: "Cennik",
+      url: "/cennik",
+    },
+  ];
