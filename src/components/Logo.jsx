fa277068c2a162af7e2c564a8dc64a43af33e62e
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-18 18:57:57 +0200

    Logo UI

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,21 @@
-
+import { Link } from "react-router-dom"
+import { logo } from "../assets"
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <Link className="flex items-center space-x-2" to="/">
+      <div className="w-[56px] h-[56px] rounded-full bg-white">
+        <img
+          src={logo}
+          alt="Diveworld Logo"
+          className="object-contain rounded-full"
+          width={56}
+          height={56}
+        />
+      </div>
+      <span className="inline-flex text-2xl font-bold text-active ">
+        Diveworld
+      </span>
+    </Link>
   )
 }
 
