487d27dbdcf621d0a21e3bd81a4a420eebdf2a77
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-18 18:56:15 +0200

    Header UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
-
+import {Logo} from "./"
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="fixed top-0 left-0 w-full z-50">
+      <div className="mx-auto p-5 flex justify-between max-w-screen-lg lg:max-w-screen-2xl">
+        <Logo/>
+        Navbar
+      </div>
+    </header>
   )
 }
 
