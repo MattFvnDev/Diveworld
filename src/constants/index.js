c222505ac87f60e9c315b1f6985e988ad6ffbfdd
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-18 12:14:02 +0200

    Defining socialLinks

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,27 +1,50 @@
 export const navigationLinks = [
-    {
-      id: "0",
-      title: "Oferta",
-      url: "/oferta",
-    },
-    {
-      id: "1",
-      title: "Szkolenia",
-      url: "/szkolenia",
-    },
-    {
-      id: "2",
-      title: "Oferta Grupowa",
-      url: "/oferta-grupowa",
-    },
-    {
-      id: "3",
-      title: "O Nas",
-      url: "o-nas",
-    },
-    {
-      id: "4",
-      title: "Cennik",
-      url: "/cennik",
-    },
-  ];
+  {
+    id: "0",
+    title: "Oferta",
+    pathname: "/oferta",
+  },
+  {
+    id: "1",
+    title: "Szkolenia",
+    pathname: "/szkolenia",
+  },
+  {
+    id: "2",
+    title: "Oferta Grupowa",
+    pathname: "/oferta-grupowa",
+  },
+  {
+    id: "3",
+    title: "O Nas",
+    pathname: "/o-nas",
+  },
+  {
+    id: "4",
+    title: "Cennik",
+    pathname: "/cennik",
+  },
+]
+
+export const socialLinks = [
+  {
+    id: "0",
+    title: "Facebook",
+    url: "https://www.facebook.com/people/Diveworldpl/100046871105720/",
+  },
+  {
+    id: "1",
+    title: "Instagram",
+    url: "https://www.instagram.com/diveworld.gozo/",
+  },
+  {
+    id: "2",
+    title: "Telegram",
+    url: "/",
+  },
+  {
+    id: "3",
+    title: "WhatsApp",
+    url: "/",
+  },
+]
