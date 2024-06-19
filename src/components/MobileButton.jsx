8e950443e2a2987cbbaa8db92ca6478e6bb222a1
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-19 21:22:15 +0200

    MobileButton styles

---
+++ src/components/MobileButton.jsx
@@ -0,0 +1,13 @@
+
+const MobileButton = () => {
+  return (
+    <button
+      
+      className="transition duration-500 delay-150 ease-in-out block rounded-full border-2 p-[12px] border-active group hover:border-default z-50 lg:hidden "
+    >
+      
+    </button>
+  )
+}
+
+export default MobileButton
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header"
 import Logo from "./Logo"
 import Footer from "./Footer"
+import MobileButton from "./MobileButton"
 
-export { Header, Logo, Footer }
+export { Header, Logo, Footer, MobileButton }
