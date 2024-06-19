c78d9527dc36166830f9f10ff6d12909844ecb54
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-19 21:26:11 +0200

    MobileMenu basic logic and functionality

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from "./Header"
 import Logo from "./Logo"
 import Footer from "./Footer"
 import MobileButton from "./MobileButton"
+import MobileMenu from "./MobileMenu"
 
-export { Header, Logo, Footer, MobileButton }
+export { Header, Logo, Footer, MobileButton, MobileMenu }
