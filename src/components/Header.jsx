928029b7f6416430628d0f270d692741d5a6b056
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-21 01:55:42 +0200

    MobileMenu and MobileNavbar refactoring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
-import { MobileMenu, Logo } from "./"
+import { MobileNavbar, MobileButton, Logo } from "./"
+import { useState } from "react"
 
 const Header = () => {
+  const [openNavigation, setOpenNavigation] = useState(false)
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false)
+    } else {
+      setOpenNavigation(true)
+    }
+  }
   return (
     <header className="fixed top-0 left-0 right-0 border-b border-active/30 z-50">
       <div className="mx-auto p-5 flex justify-between max-w-screen-2xl">
         <Logo />
-        <MobileMenu />
+        <MobileButton
+          openNavigation={openNavigation}
+          toggleNavigation={toggleNavigation}
+        />
       </div>
+      <MobileNavbar
+        toggleNavigation={toggleNavigation}
+        openNavigation={openNavigation}
+      />
     </header>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Logo from "./Logo"
 import Footer from "./Footer"
 import MobileButton from "./MobileButton"
 import MenuSvg from "./MenuSvg"
+import MobileNavbar from "./MobileNavbar"
 
-export { Header, Logo, Footer, MobileButton, MenuSvg }
+export { Header, Logo, Footer, MobileButton, MenuSvg, MobileNavbar }
