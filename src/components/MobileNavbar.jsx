8ddcca97bdce8cc71ce353f02e667bdce6266c02
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-21 14:06:29 +0200

    MobileNavbar layer and alignment adjustment

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -5,27 +5,28 @@ const MobileNavbar = ({ openNavigation, toggleNavigation }) => {
   const { pathname } = useLocation()
   return (
     <div
-      className={`fixed inset-x-0 bottom-0 top-[96px] flex w-full flex-col bg-mobilebackground duration-500 delay-150 ease-linear overflow-auto ${openNavigation ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0 -top-0"} lg:hidden`}
+      className={`fixed inset-x-0 bottom-0 h-screen bg-mobilebackground duration-500 delay-150 ease-linear ${openNavigation ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"} lg:hidden`}
     >
-      <nav className="flex flex-col justify-center items-center p-5 ">
-        <ul className="flex flex-col gap-4 w-full">
+      <nav className="relative inset-x-0 top-[96px] bottom-0 overflow-auto items-center border-t ">
+        <ul className="flex flex-col p-5 space-y-2">
           {navigationLinks.map((navigationlink) => (
-            <li key={navigationlink.id} className="text-start sm:text-center">
+            <li key={navigationlink.id} className="text-start leading-10">
               <Link
                 to={`${navigationlink.path}`}
                 onClick={toggleNavigation}
-                className={`font-bold font-code text-2xl hover:text-active transition delay-200 tracking-wide ${pathname === navigationlink.path ? "text-active" : "text-default"}`}
+                className={`font-bold font-code text-2xl hover:text-active transition delay-150 tracking-wide ${pathname === navigationlink.path ? "text-active" : "text-default"}`}
               >
                 {navigationlink.title}
               </Link>
             </li>
           ))}
-          <button
-            type="button"
-            className="w-full max-w-sm focus-ring-purple inline-block rounded-full border text-base font-bold focus-visible:ring px-8 py-4 mt-8 self-center"
+          <Link
+            to="/"
+            onClick={toggleNavigation}
+            className="w-full max-w-sm border-default inline-block text-center rounded-full tracking-wider border text-default text-2xl font-bold font-code transition delay-150 focus-visible:ring px-8 py-4 self-center hover:text-black hover:border-active hover:bg-default"
           >
             Gozo
-          </button>
+          </Link>
         </ul>
       </nav>
     </div>
