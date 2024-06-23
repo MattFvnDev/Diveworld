c4de3873297c6f92e1598fa20bac8db39d146fff
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-23 21:51:23 +0200

    Socials icons upload

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,7 @@
 import logo from "./logo/diveworld.svg"
-export { logo}
+import facebook from "./socials/facebook.svg"
+import instagram from "./socials/instagram.svg"
+import telegram from "./socials/telegram.svg"
+import whatsapp from "./socials/whatsapp.svg"
+
+export { logo, facebook, instagram, telegram, whatsapp}
