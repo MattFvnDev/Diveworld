b581dddfde51bf94009b9023a7f9b8bac06589a4
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-23 21:54:29 +0200

    Footer UI SocialIcons UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,18 @@
-import {Links} from "./"
+import { Links, SocialIcons } from "./"
 
 const Footer = () => {
   return (
-    <footer className="mx-auto max-w-screen-2xl flex items-center gap-12 px-5 py-6 sm:px-8 md:items-start">
-      <div className="flex flex-col gap-12 lg:w-full lg:flex-row lg:items-center lg:justify-between">
-        <nav>
-          <Links/>
-        </nav>
+    <footer>
+      <div className="mx-auto max-w-screen-2xl flex items-center gap-12 px-5 py-6 sm:px-8 md:items-start">
+        <div className="flex flex-col gap-8 lg:gap-5 lg:w-full lg:flex-row lg:items-center lg:justify-between">
+          <nav>
+            <Links />
+          </nav>
+          <SocialIcons />
+        </div>
       </div>
     </footer>
   )
 }
 
 export default Footer
-
-
