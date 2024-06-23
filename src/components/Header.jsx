8195e08b870debd73baee4efb1ee10ba475344bb
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-23 11:32:53 +0200

    Border top style adjustment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     }
   }
   return (
-    <header className="fixed top-0 left-0 right-0 border-b z-50">
+    <header className="fixed top-0 left-0 right-0 z-50">
       <div className="mx-auto p-5 flex justify-between max-w-screen-2xl">
         <Logo />
         <Button
