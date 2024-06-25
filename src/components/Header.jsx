1c9de59c983b892e56bd231950b04625435298c6
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-25 18:07:05 +0200

    Header Button text resize

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,9 +28,9 @@ const Header = () => {
         <Navbar />
         <Button
           href="/"
-          className="hidden lg:inline-block border-mobilebackground bg-mobilebackground text-center rounded-full tracking-wider border text-default text-xl font-bold font-code transition delay-150 px-8 py-3 hover:text-accent hover:border-default hover:bg-default "
+          className="hidden lg:inline-block border-mobilebackground bg-mobilebackground text-center rounded-full tracking-wider border text-default text-lg font-bold font-code transition delay-150 px-8 py-3 hover:text-accent hover:border-default hover:bg-default "
         >
-          Gozo
+          Dive in Gozo
         </Button>
       </div>
     </header>
