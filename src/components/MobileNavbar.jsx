75e2a16779b31688f1986f8ccf3d526c27ec0c05
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-26 22:34:58 +0200

    MobileNavbar background color change and Button

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -4,7 +4,7 @@ const MobileNavbar = ({ openNavigation, toggleNavigation }) => {
   
   return (
     <div
-      className={`fixed inset-x-0 bottom-0 top-0 flex w-full flex-col bg-mobilebackground duration-500 delay-150 ease-linear ${openNavigation ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"} lg:hidden`}
+      className={`fixed inset-x-0 bottom-0 top-0 flex w-full flex-col bg-background duration-500 delay-150 ease-linear ${openNavigation ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"} lg:hidden`}
     >
       <div className="relative top-[96px] h-[calc(100vh-96px)] overflow-auto px-5 py-3 space-y-4 border-t border-t-default">
         <NavLinks toggleNavigation={toggleNavigation}/>
@@ -12,7 +12,7 @@ const MobileNavbar = ({ openNavigation, toggleNavigation }) => {
           type="button"
           onClick={toggleNavigation}
           href="/"
-          className="inline-block w-full max-w-sm border-default bg-default text-center rounded-full tracking-wider border text-accent text-xl font-bold font-code transition delay-150 px-8 py-4 hover:text-default hover:border-active hover:bg-transparent"
+          className="inline-block w-full max-w-sm border-mobilebackground bg-mobilebackground text-center rounded-full tracking-wider border text-default text-xl font-bold font-code transition delay-150 px-8 py-4 hover:text-default hover:border-active hover:bg-transparent"
         >
           Dive in Gozo
         </Button>
