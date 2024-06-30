880c583983d75fa0d0c7540f42b53a20f542262d
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-06-30 22:21:35 +0200

    Hero UI Section with movie in background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,24 @@
+
 import { herobackground } from "../assets"
 
 const Hero = () => {
   return (
-    <section className="relative">
-      <div className="w-full h-screen">
-        <div className="absolute top-0 bottom-0 right-0 left-0 w-full h-full bg-gradient-to-b from-background/80 via-background/10 to-background"></div>
-        <div className="w-full h-screen object-cover">
+    <section className="min-h-screen">
+      <div className="z-[-1] absolute inset-0">
+        <div className="absolute z-[1] top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-background/80 via-background/10 to-background"></div>
+        <div className="z-0 object-cover w-full h-screen relative overflow-hidden">
           <video
-            className="w-full h-full object-cover"
-            src={herobackground}
+            className="object-cover w-full h-screen absolute inset-0"
             autoPlay
             loop
             muted
-          ></video>
+          >
+            <source src={herobackground} />
+          </video>
         </div>
       </div>
     </section>
   )
 }
+
 export default Hero
