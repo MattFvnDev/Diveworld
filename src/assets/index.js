2b5cda746d0f57852ca2e81747b7f712e481e414
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-02 22:22:02 +0200

    Importing carousel svg icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,40 @@ import instagram from "./socials/instagram.svg"
 import messenger from "./socials/messenger.svg"
 import whatsapp from "./socials/whatsapp.svg"
 import herobackground from "./hero/herobackground.mp4"
+import poseidon from "./carousel/poseidon.svg"
+import santi from "./carousel/santi.svg"
+import sherwood from "./carousel/sherwood.svg"
+import apex from "./carousel/apex.svg"
+import xdeep from "./carousel/xdeep.svg"
+import oms from "./carousel/oms.svg"
+import scubapro from "./carousel/scubapro.svg"
+import gralmarine from "./carousel/gralmarine.svg"
+import suunto from "./carousel/suunto.svg"
+import hollis from "./carousel/hollis.svg"
+import cressi from "./carousel/cressi.svg"
+import camaro from "./carousel/camaro.svg"
+import bare from "./carousel/bare.svg"
+import triton from "./carousel/triton.svg"
 
-export { logo, facebook, instagram, messenger, whatsapp, herobackground}
+export {
+  logo,
+  facebook,
+  instagram,
+  messenger,
+  whatsapp,
+  herobackground,
+  poseidon,
+  santi,
+  sherwood,
+  apex,
+  xdeep,
+  oms,
+  scubapro,
+  gralmarine,
+  suunto,
+  hollis,
+  cressi,
+  camaro,
+  bare,
+  triton,
+}
