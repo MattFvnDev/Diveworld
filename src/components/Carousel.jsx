65a2ed88c54a5224aa3c190448a9d327204108e1
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-02 22:25:52 +0200

    Carousel UI

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,14 @@
+const Carousel = () => {
+  return (
+    <section className="bg-default relative m-auto flex h-[5.5rem] md:h-[6rem] max-w-screen-[1920px] items-center overflow-hidden">
+      <div className="flex flex-none animate-moveCarouselIconLeft">
+        
+      </div>
+      <div className="flex flex-none animate-moveCarouselIconLeft">
+       
+      </div>
+    </section>
+  )
+}
+
+export default Carousel
