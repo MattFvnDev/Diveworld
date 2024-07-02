f801806fec1c6f37fb600e50e5c939cee83b918d
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-02 22:31:01 +0200

    Carousel U I& CarouselIcon UI passing data from carouselIcons array

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,18 @@
+import { CarouselIcon } from "./"
+import { carouselIcons } from "../constants"
+
 const Carousel = () => {
   return (
     <section className="bg-default relative m-auto flex h-[5.5rem] md:h-[6rem] max-w-screen-[1920px] items-center overflow-hidden">
       <div className="flex flex-none animate-moveCarouselIconLeft">
-        
+        {carouselIcons.map((carouselIcon) => (
+          <CarouselIcon key={carouselIcon.id} {...carouselIcon} />
+        ))}
       </div>
       <div className="flex flex-none animate-moveCarouselIconLeft">
-       
+        {carouselIcons.map((carouselIcon) => (
+          <CarouselIcon key={carouselIcon.id} {...carouselIcon} />
+        ))}
       </div>
     </section>
   )
--- src/components/CarouselIcon.jsx
+++ src/components/CarouselIcon.jsx
@@ -1,10 +1,10 @@
-const CarouselIcon = () => {
+const CarouselIcon = ({title,icon }) => {
     return (
       <img
         width="195"
         height="96"
-        src=""
-        alt=""
+        src={icon}
+        alt={title}
         className="h-14 w-auto transform-gpu sm:mx-0 sm:h-auto"
       />
     )
