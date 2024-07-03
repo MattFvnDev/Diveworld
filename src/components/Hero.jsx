5e97da97b90098dca6925294a560830a85f1b1d8
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-03 20:28:39 +0200

    Video background overlay height correction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
         </div>
       </div>
       <div className="z-[-1] absolute inset-0">
-        <div className="absolute z-[1] top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-background/80 via-background/10 to-background"></div>
+        <div className="absolute z-[1] top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-background/80 via-background/10 to-background min-h-screen"></div>
         <div className="z-0 object-cover w-full h-screen relative overflow-hidden">
           <video
             className="object-cover w-full h-screen absolute inset-0"
