babca3d68f15f6ce682ec35cf9bd31c35b971d9d
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-03 23:15:51 +0200

    Tailwind config moving custom animation for Carousel component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,25 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        "moveCarouselIconLeft": {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(-100%)",
+          },
+        },
+      },
+      animation: {
+        "moveCarouselIconLeft": "moveCarouselIconLeft 60s linear infinite",
+      },
       colors: {
         background: "#010318",
         active: "#82c9ff",
         default: "#FFFFFF",
         mobilebackground: "#4d117a",
-        accent:"#ff6726"
+        accent: "#ff6726",
       },
       fontFamily: {
         sans: ["var(--font-sora)", ...fontFamily.sans],
