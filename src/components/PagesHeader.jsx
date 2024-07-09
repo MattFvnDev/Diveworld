0dc1b926290093f3dfa7955c82584defe9612c1b
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-09 22:32:27 +0200

    PagesHeader UI

---
+++ src/components/PagesHeader.jsx
@@ -0,0 +1,13 @@
+const PagesHeader = () => {
+  return (
+    <section>
+      <div className="mx-auto px-5 max-w-screen-2xl">
+        <div className="flex flex-col justify-end w-full relative h-[15rem]">
+          PagesHeader
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default PagesHeader
--- src/components/index.js
+++ src/components/index.js
@@ -14,6 +14,7 @@ import Hero from "./Hero"
 import Carousel from "./Carousel"
 import CarouselIcon from "./CarouselIcon"
 import ChooseUs from "./ChooseUs"
+import PagesHeader from "./PagesHeader"
 
 export {
   Header,
@@ -31,5 +32,6 @@ export {
   Hero,
   Carousel,
   CarouselIcon,
-  ChooseUs
+  ChooseUs,
+  PagesHeader
 }
