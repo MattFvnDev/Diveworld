8ef2f5545c053f0546e3f97cab2efa879ee8ddd2
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-11 21:58:46 +0200

    ChooseUsContent UI

--- src/components/index.js
+++ src/components/index.js
@@ -14,6 +14,7 @@ import Hero from "./Hero"
 import Carousel from "./Carousel"
 import CarouselIcon from "./CarouselIcon"
 import ChooseUs from "./ChooseUs"
+import ChooseUsContent from "./ChooseUsContent"
 import PagesHeader from "./PagesHeader"
 import Breadcrumbs from "./Breadcrumbs"
 
@@ -34,6 +35,7 @@ export {
   Carousel,
   CarouselIcon,
   ChooseUs,
+  ChooseUsContent,
   PagesHeader,
   Breadcrumbs,
 }
