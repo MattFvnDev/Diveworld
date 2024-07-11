fb267a1587bec4437fc3d8331c55450273326d62
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-11 22:14:08 +0200

    ChooseUsContent Destructuring Props

--- src/components/ChooseUsContent.jsx
+++ src/components/ChooseUsContent.jsx
@@ -1,10 +1,10 @@
-const ChooseUsContent = () => {
-    return (
-      <div className="flex flex-col space-y-2 max-w-2xl">
-        <h2 className="font-semibold text-xl"></h2>
-        <p className="font-medium text-default/70"></p>
-      </div>
-    )
-  }
-  
-  export default ChooseUsContent
+const ChooseUsContent = ({ subTitle, stage }) => {
+  return (
+    <div className="flex flex-col space-y-2 max-w-2xl">
+      <h2 className="font-semibold text-xl">{subTitle}</h2>
+      <p className="font-medium text-default/70">{stage}</p>
+    </div>
+  )
+}
+
+export default ChooseUsContent
