5b9346b484abc6561a87a7217bf731e1ec4777de
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-11 21:57:15 +0200

    ChooseUs UI

--- src/components/ChooseUs.jsx
+++ src/components/ChooseUs.jsx
@@ -1,7 +1,18 @@
-
 const ChooseUs = () => {
   return (
-    <div>ChooseUs</div>
+    <section className="mx-auto max-w-screen-2xl p-5 ">
+      <h1 className="text-accent font-semibold text-2xl mt-8 mb-8 md:text-3xl xl:text-4xl md:mb-12 xl:mb-16">
+        ChooseUs
+      </h1>
+      <div className="flex flex-col-reverse md:flex-row w-full md:gap-8 lg:gap-12 xl:gap-16 2xl:gap-20 ">
+        <div className="flex flex-col space-y-8 mt-8 items-center sm:flex-row sm:space-y-0 sm:mt-0 sm:justify-between sm:space-x-4 md:flex-col md:items-baseline md:w-1/3 md:justify-start md:space-y-12">
+          
+        </div>
+        <div className="min-h-[480px] flex flex-col gap-y-6 md:w-2/3 ">
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
