2c50726013b0eed93c76354f24239e21eec122e9
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-12 22:03:37 +0200

    Header Gozo Button style change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import { MobileNavbar, Logo, Navbar, Button, MenuSvg } from "./"
 import { useState } from "react"
+import { MobileNavbar, Logo, Navbar, Button, MenuSvg } from "./"
+import { malta } from "../assets"
 
 const Header = () => {
   const [openNavigation, setOpenNavigation] = useState(false)
@@ -28,9 +29,14 @@ const Header = () => {
         <Navbar />
         <Button
           href="/"
-          className="hidden lg:inline-block min-w-48 border-accent bg-accent text-center rounded-full tracking-widest border text-default text-lg font-bold font-code transition delay-150 px-8 py-3 hover:text-accent hover:border-active hover:bg-default "
+          className="hidden lg:inline-flex relative items-center justify-center overflow-hidden min-w-48 duration-700 ease-out border-default bg-default/30 text-center rounded-full tracking-widest border-2 text-lg font-bold font-code transition delay-200 px-8 py-3 group hover:border-active"
         >
-          Gozo
+          <span className="absolute inset-0 flex items-center justify-center duration-500 -translate-x-full group-hover:translate-x-0">
+            <img src={malta} alt="An icon of Maltese Archipelago" />
+          </span>
+          <span className="absolute flex items-center text-lg font-semibold justify-center w-full h-full text-default transition-all duration-300 transform group-hover:translate-x-full">
+            Gozo
+          </span>
         </Button>
       </div>
     </header>
