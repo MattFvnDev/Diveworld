e9d9af4019be71d65618b8c2805463e09e9db8ab
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-16 21:55:53 +0200

    ChooseUs Heading element adjustment

--- src/components/ChooseUs.jsx
+++ src/components/ChooseUs.jsx
@@ -11,16 +11,18 @@ const ChooseUs = () => {
           <Glow />
         </div>
       </div>
-      <h1 className="text-accent font-semibold text-2xl mt-4 mb-8 md:text-3xl xl:text-4xl md:mb-12 xl:mb-16">
-        {mainTitle}
-      </h1>
+      <div className="max-w-[50rem] mx-auto mb-12 lg:mb-20 lg:text-center">
+        <h2 className="text-accent font-semibold mt-4 mb-8 md:mb-12 xl:mb-16 text-2xl leading-none md:text-3xl lg:text-4xl xl:text-5xl">
+          {mainTitle}
+        </h2>
+      </div>
       <div className="flex flex-col-reverse md:flex-row w-full md:gap-8 lg:gap-10 xl:gap-12 2xl:gap-16 ">
         <div className="flex flex-col space-y-8 mt-8 items-center sm:flex-row sm:space-y-0 sm:mt-0 sm:justify-between sm:space-x-4 md:flex-col md:items-baseline md:w-2/5 md:justify-start md:space-y-12">
           {content.map((item, itemIndex) => (
             <Button
               key={itemIndex}
               onClick={() => setIndex(itemIndex)}
-              className={`flex justify-center rounded-full w-full max-w-64 border-2 border-accent transition-all duration-300 px-8 py-4 md:mt-16 hover:border-default group ${index === itemIndex && "bg-mobilebackground border-active"}`}
+              className={`flex justify-center rounded-full w-full max-w-64 border-2 border-accent transition-all duration-300 px-8 py-4  hover:border-default group ${index === itemIndex && "bg-mobilebackground border-active"}`}
             >
               <img
                 src={item.icon}
