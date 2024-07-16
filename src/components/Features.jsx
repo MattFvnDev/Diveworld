ecc5214c394bad8b56f99b2df17c8374f215b55b
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-16 21:40:52 +0200

    Features UI developing Card and List

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,6 @@
-import { FeatureImages } from "./"
+import { Link } from "react-router-dom"
+import { Card, FeatureImages, FeaturesList } from "./"
+import { diveWorldFeatures } from "../constants/index"
 
 const Features = () => {
   return (
@@ -26,8 +28,8 @@ const Features = () => {
                   y1="0%"
                   y2="100%"
                 >
-                  <stop offset="0%" stop-color="#82c9ff"></stop>
-                  <stop offset="100%" stop-color="#3d5ff8"></stop>
+                  <stop offset="0%" stopColor="#82c9ff"></stop>
+                  <stop offset="100%" stopColor="#3d5ff8"></stop>
                 </linearGradient>
               </defs>
             </svg>
@@ -53,8 +55,8 @@ const Features = () => {
                   y1="0%"
                   y2="100%"
                 >
-                  <stop offset="0%" stop-color="#82c9ff"></stop>
-                  <stop offset="100%" stop-color="#3d5ff8"></stop>
+                  <stop offset="0%" stopColor="#82c9ff"></stop>
+                  <stop offset="100%" stopColor="#3d5ff8"></stop>
                 </linearGradient>
               </defs>
             </svg>
@@ -62,6 +64,30 @@ const Features = () => {
           <h2 className="text-2xl leading-none md:text-3xl lg:text-4xl xl:text-5xl">
             Zanurkuj raz, skorzystaj na zawsze
           </h2>
+        </div>
+        <div className="mx-auto max-w-screen-xl lg:max-w-screen-2xl">
+          <div className="grid gap-10 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+            {diveWorldFeatures.map((feature) => (
+              <Card
+                key={feature.id}
+                className="border w-[18rem] max-lg:w-full h-full px-6 border-accent rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-default [&>h4]:even:text-accent [&>h4]:last:text-active"
+              >
+                <h4 className="mb-4 text-3xl lg:text-4xl font-grotesk">
+                  {feature.title}
+                </h4>
+                <p className="min-h-[4rem] mb-3 text-default/50 lg:text-lg">
+                  {feature.description}
+                </p>
+                <Link
+                  to={`${feature.path}`}
+                  className="relative rounded-full inline-flex items-center justify-center h-11 transition-colors bg-accent border border-accent px-7 w-full mb-6 hover:bg-transparent hover:text-active font-semibold tracking-wider"
+                >
+                  Szczegóły
+                </Link>
+                <FeaturesList feature={feature}/>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </section>
