b6ff080cf9bd4a848a92b78c4f1356b176035a04
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-17 21:29:03 +0200

    RootLayout overflow adjustment

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,7 +3,7 @@ import { Header, Footer } from "../components"
 
 const RootLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col overflow-hidden">
       <Header />
       <main className="flex-1">
         <Outlet />
