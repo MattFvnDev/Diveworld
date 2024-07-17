d5434e55ca7e83fd66d207fc675a9344d1977f88
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-17 19:31:08 +0200

    Button onClick atribute add to renderLink

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
-const Button = ({ className, to, onClick, children}) => {
-    const renderButton = () => (
-      <button onClick={onClick} className={className}>
-        {children}
-      </button>
-    );
-  
-    const renderLink = () => (
-      <Link to={to} className={className}>
-        {children}
-      </Link>
-    );
-  
-    return to ? renderLink() : renderButton();
-  };
+import { Link } from "react-router-dom"
+const Button = ({ className, to, onClick, children, type }) => {
+  const renderButton = () => (
+    <button onClick={onClick} type={type} className={className}>
+      {children}
+    </button>
+  )
 
-export default Button;
+  const renderLink = () => (
+    <Link to={to} onClick={onClick} className={className}>
+      {children}
+    </Link>
+  )
+
+  return to ? renderLink() : renderButton()
+}
+
+export default Button
