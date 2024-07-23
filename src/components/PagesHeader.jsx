dec37f6673a0b2000d563a6180bc0b2b68727280
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-24 00:00:02 +0200

    PagesHeader hight adjustment

--- src/components/PagesHeader.jsx
+++ src/components/PagesHeader.jsx
@@ -3,7 +3,7 @@ const PagesHeader = () => {
   return (
     <section>
       <div className="mx-auto px-5 max-w-screen-2xl">
-        <div className="flex flex-col justify-end w-full relative h-[15rem]">
+        <div className="flex flex-col justify-end w-full relative h-[10rem]">
           <Breadcrumbs/>
         </div>
       </div>
