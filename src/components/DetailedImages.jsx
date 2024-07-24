997a923bc97ff06534306f9de01247e8a1d3d5ba
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-24 19:37:23 +0200

    DetailedImages hover effect

--- src/components/DetailedImages.jsx
+++ src/components/DetailedImages.jsx
@@ -13,14 +13,14 @@ const DetailedImages = ({ img, title, otherImgs }) => {
         alt=""
         className="object-cover object-center lg:h-4/5 aspect-square md:aspect-video rounded-[1rem] shadow-[0px_0px_25px_1px_rgba(255,255,255,1)]"
       />
-      <div className="flex justify-center items-center gap-6 md:gap-12 xl:gap-16 my-16">
+      <div className="flex justify-center items-center gap-6 md:gap-12 xl:gap-16 my-16 relative">
         {otherImgs.map((img, index) => (
           <img
             key={index}
             src={img}
             alt={`An image of ${title}`}
             onClick={changeImage}
-            className="w-20 h-20 md:w-24 lg:w-28 lg:h-24 xl:w-32 xl:h-28 object-cover cursor-pointer rounded-[1rem] transition-colors duration-300 delay-150 hover:border hover:border-active "
+            className="w-20 h-20 md:w-24 lg:w-28 lg:h-24 xl:w-32 xl:h-28 object-cover cursor-pointer rounded-[1rem] border transition duration-500 ease-in-out hover:scale-110 delay-150 hover:border-active"
           />
         ))}
       </div>
