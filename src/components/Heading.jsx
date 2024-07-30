8f9ccdc7013ca5de19b4a2d3beb51d80e30dee88
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-07-30 21:36:08 +0200

    Heading additional paramater

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,14 +1,21 @@
-import {Tag} from "./"
+import { Tag } from "./"
 
-const Heading = ({ className, title, tag }) => {
-    return (
-      <div
-        className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
-      >
-        {tag && <Tag className="mb-4 md:justify-center">{tag}</Tag>}
-        {title && <h2 className="text-2xl leading-none md:text-3xl lg:text-4xl xl:text-5xl">{title}</h2>}
-      </div>
-    )
-  }
-  
-  export default Heading
+const Heading = ({ className, title, tag, text }) => {
+  return (
+    <div
+      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
+    >
+      {tag && (
+        <Tag className="mb-4 md:justify-center text-default/70">{tag}</Tag>
+      )}
+      {title && (
+        <h2 className="text-2xl leading-none md:text-3xl lg:text-4xl xl:text-5xl">
+          {title}
+        </h2>
+      )}
+      {text && <p className=" text-base mt-4 text-default/70">{text}</p>}
+    </div>
+  )
+}
+
+export default Heading
