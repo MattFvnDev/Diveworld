f20ae49c4e85d7669f78cadae0066ae274015405
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-08-01 21:50:50 +0200

    DetailedCourse UI

--- src/pages/DetailedCourse.jsx
+++ src/pages/DetailedCourse.jsx
@@ -1,8 +1,21 @@
-import { PagesHeader } from "../components"
+import { Card, Heading, PagesHeader } from "../components"
+import { useParams } from "react-router-dom"
+import { diveWorldCourses } from "../constants"
+
 const DetailedCourse = () => {
+  const { id } = useParams()
   return (
     <>
-    <PagesHeader/>
+      <PagesHeader />
+      {diveWorldCourses
+        .filter((course) => course.id === id)
+        .map((course) => (
+          <section key={course.id} className="py-10 lg:py-16 xl:pt-20 xl:pb-28">
+            <Card className="max-w-screen-2xl mx-auto px-5 flex flex-col gap-4">
+              <Heading title={course.title} text={course.content} />
+            </Card>
+          </section>
+        ))}
     </>
   )
 }
