0c079ef3fe789f693ffb89bc603007c812e19c21
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-08-04 20:24:35 +0200

    Implementing Heading in Offer

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,10 +1,18 @@
-import { Mask, PagesHeader } from "../components"
+import { Heading, Mask, PagesHeader } from "../components"
 
 const Offer = () => {
   return (
     <>
       <Mask />
       <PagesHeader />
+      <section>
+        <div className="px-5 pt-20 text-center">
+          <Heading
+            title="Oferta Nurkowa"
+            text="Nasza oferta skierowana jest do osób, które nigdy wcześniej nie miały kontaktu z nurkowaniem, a także dla początkujących i doświadczonych nurków. Przeprowadzamy nurkowania w małych grupach, dostosowując je do poziomu i preferancji naszych klientów. W naszej ofercie każdy znajdzie coś dla siebie, przeprowadzamy nurkowania głębokie, jaskiniowe, wrakowe oraz z wykorzystaniem konfiguracji bocznej, tj. Sidemount."
+          />
+        </div>
+      </section>
     </>
   )
 }
