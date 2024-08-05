5efad73cfd2bd222645f700a3320358758047490
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-08-05 18:53:42 +0200

    implementing Card in BestCourses UI

--- src/components/BestCourses.jsx
+++ src/components/BestCourses.jsx
@@ -1,6 +1,8 @@
-import Heading from "./Heading"
-
+import { diveWorldCourses } from "../constants"
+import {Heading, Card} from "./"
 const BestCourses = () => {
+  const bestCourses = diveWorldCourses.filter((course) => course.subCategory)
+
   return (
     <section className="relative py-10 lg:py-16 xl:py-20">
       <div className="max-w-screen-2xl mx-auto px-5 ">
@@ -8,6 +10,26 @@ const BestCourses = () => {
           tag="Szkolenia z Diveworld"
           title="Najczęściej wybierane szkolenia"
         />
+        <div
+          className="flex gap-12 overflow-hidden whitespace-nowrap scroll-smooth relative"
+        >
+          {bestCourses.map((course) => (
+            <Card
+              key={course.id}
+              to={course.path}
+              className="cursor-pointer border-2 border-default hover:border-accent duration-300 ease-in-out"
+            >
+              <div className="w-64">
+                <img
+                  src={course.img}
+                  alt={`An image of ${course.title}`}
+                  className="object-contain size-full"
+                />
+              </div>
+            </Card>
+          ))}
+        </div>
+       
       </div>
     </section>
   )
