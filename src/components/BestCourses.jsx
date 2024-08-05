2d58428048cd71bb46ef066fdff928eb360c09fb
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-08-05 18:51:55 +0200

    BestCourses UI

--- src/components/BestCourses.jsx
+++ src/components/BestCourses.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Heading from "./Heading"
 
 const BestCourses = () => {
   return (
-    <div>BestCourses</div>
+    <section className="relative py-10 lg:py-16 xl:py-20">
+      <div className="max-w-screen-2xl mx-auto px-5 ">
+        <Heading
+          tag="Szkolenia z Diveworld"
+          title="Najczęściej wybierane szkolenia"
+        />
+      </div>
+    </section>
   )
 }
 
