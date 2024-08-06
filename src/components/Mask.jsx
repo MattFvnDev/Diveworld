f2a9e931eeed89e43f1dd405c04f2b6068fc48ec
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-08-06 19:00:15 +0200

    Mask style adjustment

--- src/components/Mask.jsx
+++ src/components/Mask.jsx
@@ -1,11 +1,15 @@
-const Mask = ({img}) => {
+const Mask = ({ img }) => {
   return (
     <div className="z-[-1] absolute inset-0">
-        <div className="absolute z-[1] inset-0 bg-gradient-to-b from-background/80 from-5% via-background/10 to-background"></div>
-        <div className="z-0 w-full h-screen relative overflow-hidden ">
-          <img src={img} alt={`An image of ${img}`} className="object-cover" />
-        </div>
+      <div className="absolute z-[1] inset-0 bg-gradient-to-b from-background/80 from-5% via-background/10 to-background"></div>
+      <div className="z-0 w-full h-screen relative overflow-hidden ">
+        <img
+          src={img}
+          alt={`An image of ${img}`}
+          className="object-cover size-full absolute inset-0"
+        />
       </div>
+    </div>
   )
 }
 
