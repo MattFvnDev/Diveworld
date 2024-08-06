27d35d77c397cf14bcbc617bbbbb99c13fc3cf14
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-08-06 18:51:09 +0200

    Mask UI reusable component

--- src/components/Mask.jsx
+++ src/components/Mask.jsx
@@ -1,8 +1,10 @@
-
-const Mask = () => {
+const Mask = ({img}) => {
   return (
-    <div className="z-[-1] relative">
-        <div className="absolute inset-x-0 h-[50rem] bg-bg-image bg-cover"></div>
+    <div className="z-[-1] absolute inset-0">
+        <div className="absolute z-[1] inset-0 bg-gradient-to-b from-background/80 from-5% via-background/10 to-background"></div>
+        <div className="z-0 w-full h-screen relative overflow-hidden ">
+          <img src={img} alt={`An image of ${img}`} className="object-cover" />
+        </div>
       </div>
   )
 }
