ab79bfd1ecc858e4886634947ca23aadd06690e6
Mateusz Grzelec <m.grzelec93@gmail.com>
2024-08-07 22:41:02 +0200

    Mask UI reusable component video/image

--- src/components/Mask.jsx
+++ src/components/Mask.jsx
@@ -1,5 +1,5 @@
-const Mask = ({ img }) => {
-  return (
+const Mask = ({ img, video }) => {
+  const renderImage = () => (
     <div className="z-[-1] absolute inset-0">
       <div className="absolute z-[1] inset-0 bg-gradient-to-b from-background/80 from-5% via-background/10 to-background"></div>
       <div className="z-0 w-full h-screen relative overflow-hidden ">
@@ -11,6 +11,23 @@ const Mask = ({ img }) => {
       </div>
     </div>
   )
+  const renderVideo = () => (
+    <div className="z-[-1] absolute inset-0">
+      <div className="absolute z-[1] top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-background/80 via-background/10 to-background min-h-screen"></div>
+      <div className="z-0 object-cover w-full h-screen relative overflow-hidden">
+        <video
+          className="object-cover w-full h-screen absolute inset-0"
+          autoPlay
+          loop
+          muted
+        >
+          <source src={video} />
+        </video>
+      </div>
+    </div>
+  )
+
+  return video ? renderVideo() : renderImage()
 }
 
 export default Mask
